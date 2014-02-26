9b511a352d4042308b3f7045c56b7131467ee290
Cody <goodmorningcody@gmail.com>
2014-02-26 19:08:57 +0900

    Added ProfileActivity

--- src/com/goodmorningcody/androidstudy/ProfileActivity.java
+++ src/com/goodmorningcody/androidstudy/ProfileActivity.java
@@ -1,6 +1,29 @@
 package com.goodmorningcody.androidstudy;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ProfileActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.profile_activity);
+	    
+	    //findViewById(R)
+	    Button goToFacebook = (Button)findViewById(R.id.profile_go_to_facebook);
+	    goToFacebook.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/okieyun"));
+				startActivity(browserIntent);
+			}
+		});
+	}
 }
